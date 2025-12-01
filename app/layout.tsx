be84946e679349ba04f728ef51b24ad3c73712f4
Oliver Dean Johnston <oliverdeanjohnston@gmail.com>
2025-12-01 20:16:10 +0000

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
+import { personalData } from '@/data/data';
 import { Navbar } from '@/components/navbar';
 import { ThemeProvider } from '@/components/theme-provider';
 
@@ -11,8 +12,30 @@ const supreme = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "oliver's website",
-  description: "oliver's website",
+  title: {
+    default: `${personalData.name} | Full Stack Web Developer`,
+    template: `%s | ${personalData.name}`,
+  },
+  description: personalData.description,
+  openGraph: {
+    title: `${personalData.name} | Full Stack Web Developer`,
+    description: personalData.description,
+    images: [personalData.avatarUrl],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/images/me.avif',
+  },
 };
 
 export default function RootLayout({
